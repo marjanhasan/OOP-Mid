29df1e7989c280f6f2bcf8223639d866c67991a3
marjanhasan <moonmarjanhasan@gmail.com>
2023-10-05 21:35:09 +0600

    Star_Cinema and Hall class added

--- cinema.py
+++ cinema.py
@@ -1,3 +1,18 @@
 class Star_Cinema:
+    hall_list = []
+
     def __init__(self) -> None:
-        self.hall_list = []
+        pass
+
+    def entry_hall(self, rows, cols, hall_no):
+        hall = Hall(rows, cols, hall_no)
+        self.hall_list.append(hall)
+
+
+class Hall:
+    def __init__(self, rows, cols, hall_no) -> None:
+        self.seats = {}
+        self.show_list = ()
+        self.rows = rows
+        self.cols = cols
+        self.hall_no = hall_no
