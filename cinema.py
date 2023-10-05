2642a119b2071f1fb4857933ac922b65d349f9f4
marjanhasan <moonmarjanhasan@gmail.com>
2023-10-06 02:51:54 +0600

    entry_show, view_show_list & view_available_seats method are revised and bug fixed

--- cinema.py
+++ cinema.py
@@ -19,13 +19,20 @@ class Hall:
         self.hall_no = hall_no
 
     def entry_show(self, id, movie_name, time):
-        if id not in self.seats:
+        if len(self.show_list) == 0:
             show = (id, movie_name, time)
             self.show_list.append(show)
             seat = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
             self.seats[id] = seat
-        else:
-            print(f"Show ID '{id}' already exists.")
+            return
+        for show in self.show_list:
+            if show[0] == id:
+                print(f"Show ID '{id}' already exists.")
+                return
+        show = (id, movie_name, time)
+        self.show_list.append(show)
+        seat = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
+        self.seats[id] = seat
 
     def book_seats(self, id, bookings):  # todo bookings list
         if id not in self.seats:
@@ -43,16 +50,36 @@ class Hall:
             print(f"Invalid show ID. Please try again!")
 
     def view_show_list(self):
-        if 0 != len(self.show_list):
+        if len(self.show_list) != 0:
             print("Availabe Shows are down below")
             for show in self.show_list:
-                print(show)
+                print(f"Show ID: {show[0]}, Name: {show[1]}, Time: {show[2]}")
         else:
             print("Oops! There are no available shows today.")
 
     def view_available_seats(self, id):
         if id in self.seats:
             for row in self.seats[id]:
-                print(row)
+                for element in row:
+                    if element == 1:
+                        print("B", end=" ")
+                    else:
+                        print(element, end=" ")
+                print()
         else:
-            print(f"There is not availble seats on show ID {id}")
+            print(f"Oops! Invalid show ID: {id}. Please try again")
+        return
+
+
+banalata = Hall(3, 3, 2)
+banalata.entry_show("s1", "jawan", "2:00")
+banalata.entry_show("s1", "jawan", "2:00")
+banalata.entry_show("s3", "dawan", "4:00")
+banalata.entry_show("s4", "dawan", "4:00")
+banalata.entry_show("s1", "dawan", "4:00")
+banalata.entry_show("s1", "dawan", "4:00")
+banalata.entry_show("s3", "dawan", "4:00")
+banalata.view_show_list()
+banalata.view_available_seats("s3")
+for s in banalata.show_list:
+    print(s[0])
