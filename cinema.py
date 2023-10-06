f2572e46d297fafe78b708fa72b5a61c72c75204
marjanhasan <moonmarjanhasan@gmail.com>
2023-10-06 16:24:15 +0600

    Replica added successfully

--- cinema.py
+++ cinema.py
@@ -1,5 +1,5 @@
 class Star_Cinema:
-    hall_list = []
+    _hall_list = []
 
     def __init__(self) -> None:
         pass
@@ -12,74 +12,76 @@ class Star_Cinema:
 
 class Hall:
     def __init__(self, rows, cols, hall_no) -> None:
-        self.seats = {}
-        self.show_list = []
+        self.__seats = {}
+        self.__show_list = []
         self.rows = rows
         self.cols = cols
-        self.hall_no = hall_no
+        self._hall_no = hall_no
 
     def entry_show(self, id, movie_name, time):
-        if len(self.show_list) == 0:
+        if len(self.__show_list) == 0:
             show = (id, movie_name, time)
-            self.show_list.append(show)
+            self.__show_list.append(show)
             seat = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
-            self.seats[id] = seat
+            self.__seats[id] = seat
             return
-        for show in self.show_list:
+        for show in self.__show_list:
             if show[0] == id:
                 print(f"Show ID '{id}' already exists.")
                 return
         show = (id, movie_name, time)
-        self.show_list.append(show)
+        self.__show_list.append(show)
         seat = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
-        self.seats[id] = seat
+        self.__seats[id] = seat
 
     def book_seats(self, id, bookings):
-        if id in self.seats:
+        if id in self.__seats:
             for book in bookings:
                 row = int(book[0])
                 col = int(book[1])
                 if (row < 0) or (self.rows <= row) or (col < 0) or (self.cols <= col):
-                    print(f"Invalid seat position ({row}, {col}). Please try again!")
+                    print(f"\nInvalid seat position ({row}, {col}). Please try again!")
                     continue
-                if self.seats[id][row][col] == 0:
-                    self.seats[id][row][col] = 1
+                if self.__seats[id][row][col] == 0:
+                    self.__seats[id][row][col] = 1
                     print(
-                        f"Seat ({row}, {col}) is booked sucessfully for the show ID {id}"
+                        f"\nSeat ({row}, {col}) is booked sucessfully for the show ID {id}"
                     )
                 else:
-                    print(f"Seat ({row}, {col}) is already booked for the show ID {id}")
+                    print(
+                        f"\nSeat ({row}, {col}) is already booked for the show ID {id}"
+                    )
 
         else:
-            print(f"Invalid show ID: {id}. Please try again!")
+            print(f"\nInvalid show ID: {id}. Please try again!")
 
     def view_show_list(self):
-        if len(self.show_list) != 0:
-            print("Availabe Shows are down below")
-            for show in self.show_list:
-                print(f"Show ID: {show[0]}, Name: {show[1]}, Time: {show[2]}")
+        if len(self.__show_list) != 0:
+            print("\nAvailabe Shows are down below")
+            for show in self.__show_list:
+                print(f"\nShow ID: {show[0]}, Name: {show[1]}, Time: {show[2]}")
         else:
-            print("Oops! There are no available shows today.")
+            print("\nOops! There are no available shows today.")
 
     def view_available_seats(self, id):
-        if len(self.show_list) == 0:
-            print("Oops! There are no available shows today.")
+        if len(self.__show_list) == 0:
+            print("\nOops! There are no available shows today.")
             return
-        if id in self.seats:
-            print("Availabe seats are down below")
-            for row in self.seats[id]:
+        if id in self.__seats:
+            print("\nAvailabe seats are down below")
+            for row in self.__seats[id]:
                 for element in row:
                     print(element, end=" ")
                 print()
         else:
-            print(f"Oops! Invalid show ID: {id}. Please try again")
+            print(f"\nOops! Invalid show ID: {id}. Please try again")
 
     def __repr__(self) -> str:
         print("\nAvailabe Shows are down below")
-        for show in self.show_list:
+        for show in self.__show_list:
             print(f"Show ID: {show[0]}, Name: {show[1]}, Time: {show[2]}")
         print("\nAvailabe seats are down below")
-        for show_id, seat_arrangement in self.seats.items():
+        for show_id, seat_arrangement in self.__seats.items():
             print(f"Available seats for show id: {show_id}")
             for row in seat_arrangement:
                 print(" ".join(map(str, row)))
@@ -89,9 +91,35 @@ class Hall:
 
 banalata = Hall(6, 6, 2)
 banalata.entry_show("111", "Jawan", "6/10/23 10:00AM")
-banalata.entry_show("112", "Dhawan", "6/10/23 12:00PM")
-banalata.book_seats("111", [(0, 0), (0, 1)])
-t = 3
-while t != 0:
+banalata.entry_show("112", "Barbie", "6/10/23 12:00PM")
+banalata.entry_show("113", "Oppenheimer", "6/10/23 2:00PM")
+while True:
     print(banalata)
-    t -= 1
+
+    print("Options:\n")
+    print("1: Book Seats")
+    print("2: Entry Show")
+    print("3: Exit")
+
+    ch = int(input("\nEnter Options: "))
+
+    if ch == 1:
+        id = input("Please Enter show ID: ")
+        seats = int(input("How many seats do you want (in number): "))
+        bookings = []
+        while seats != 0:
+            print("Please Enter your seats like (row, column) separately asked")
+            row = input("Please enter row: ")
+            col = input("Please enter col: ")
+            bookings.append((row, col))
+            seats -= 1
+        banalata.book_seats(id, bookings)
+    elif ch == 2:
+        id = input("Please Enter show ID: ")
+        movie_name = input("Please enter show name: ")
+        time = input("Please enter show time: ")
+        banalata.entry_show(id, movie_name, time)
+    elif ch == 3:
+        break
+    else:
+        print("\nOops Invalid options. Please select between 1,2 or 3")
